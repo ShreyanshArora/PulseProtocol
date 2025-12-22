1769a7bfb11e58238bf71a454794fe3bf7562429
Shreyansh Arora <144546852+ShreyanshArora@users.noreply.github.com>
2025-12-22 16:27:05 +0530

    Initial Commit

---
+++ PulseProtocol2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PulseProtocol2.0
+//
+//  Created by Shreyansh on 22/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PulseProtocol2.0/PulseProtocol2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  PulseProtocol2_0App.swift
+//  PulseProtocol2.0
+//
+//  Created by Shreyansh on 22/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseProtocol2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
